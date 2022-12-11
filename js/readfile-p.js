bca5a938aefd86ca733d5d88c60c66bc2362adcb
chen <cow8352@gmail.com>
2022-12-11 11:08:06 +0800

    aaa

--- js/readfile-p.js
+++ js/readfile-p.js
@@ -14,7 +14,16 @@
 const fs = require('fs');
 let p = new Promise((resolve, reject) => {
     fs.readFile('test.txt', 'utf-8', (err, data) =>{
-        resolve(data)
+      if (err) {
+        // 如果 err 有值，表示有錯誤發生
+        // 這裡應該要處理錯誤
+        reject(err);
+        // console.error('發生錯誤了', err);
+      } else {
+        // 進來這裡，表示 err 是空的 (可能是 null)
+        resolve(data);
+        // console.log('成功讀到資料:', data);
+      }
     })
 });
 
